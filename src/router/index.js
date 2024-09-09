0f761cdea682682f8c800b6574e17386e4717557
judy <judychang789@gmail.com>
2024-09-10 01:15:05 +0800

    router修改第2次

--- src/router/index.js
+++ src/router/index.js
@@ -18,7 +18,7 @@ const router = createRouter({
     //   component: () => import('../views/AboutView.vue')
     // },
     {
-      path: '/',
+      path: '/todolist',
       name: 'todolist',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
