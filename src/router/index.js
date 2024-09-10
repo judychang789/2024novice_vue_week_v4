369207ff750860225595e0d4382e4812229ab9ec
judy <judychang789@gmail.com>
2024-09-10 20:53:15 +0800

    routes修改

--- src/router/index.js
+++ src/router/index.js
@@ -19,7 +19,7 @@ const router = createRouter({
     // },
     {
       //path: '/todolist',
-      path: '/todolist',
+      path: '/',
       name: 'todolist',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
